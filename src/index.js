762d3e1e84d6a266b6a1429064e78dbc261955b9
Nur Digdem Deyirmenci <150827069+DigdemD@users.noreply.github.com>
2023-12-05 18:38:25 +0000

    Digdem created Customer class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log("Hi coyote, let's have some JavaScript fun!")
+
+class Customer {
+  name
+  surname
+  email
+
+  constructor(name, surname, email) {
+    this.name = name
+    this.surname = surname
+    this.email = email
+  }
+}
+
+let veronica = new Customer(Veronica, Barredo, veronica@mexicana.com)
+let alex = new Customer(Alex, Kimoto, alex@gmail.com)
