923af29aa11e0bd5550c4b56a2b0ca4d83834655
Nur Digdem Deyirmenci <150827069+DigdemD@users.noreply.github.com>
2023-12-19 17:17:48 +0000

    introducing internalBalanceTransfer and eXternalBalanceTransfer to allow user basic transfer actions

--- src/account.js
+++ src/account.js
@@ -4,29 +4,30 @@ class Account {
   #balance = 0
   #transactions = []
   #overdraft = 0
+  status = 'Active'
+  name = ''
   static accIdCounter = 1
   constructor(currency, owner) {
-    this.accountId = Account.accIdCounter++
+    this.accId = Account.accIdCounter++
     this.currency = currency
     this.owner = owner
   }
   #setBalance = newAmount => {
-    if (newAmount < -this.#overdraft) throw new Error(`Balance can not go below -${this.#overdraft}`)
+    //if (newAmount < -this.#overdraft) throw new Error(`Balance can not go below -${this.#overdraft}`)
     this.#balance = newAmount
   }
-
   get balance() {
     return this.#balance
   }
 
   deposit(amount) {
-    if (amount <= 0) throw new Error(`Amount must be positive number`)
+    //if (amount <= 0) throw new Error(`Amount must be positive number`)
     this.#transactions.push(amount)
     this.#setBalance(this.#balance + amount)
   }
 
   withdraw(amount) {
-    if (amount <= 0) throw new Error(`Amount must be positive number`)
+    //if (amount <= 0) throw new Error(`Amount must be positive number`)
     this.#transactions.push(amount)
     this.#setBalance(this.#balance - amount)
     this.#setBalance
--- src/user.js
+++ src/user.js
@@ -20,21 +20,51 @@ class User {
   tradeVolume = 0 //USD
   id = User.idCounter++
 
-  openAccount(currency) {
+  openAccount(currency, accId) {
     const newAccount = new Account(currency, this.id)
     this.accounts.push(newAccount)
     return newAccount
   }
+
   executeTrade(buyAmount, sellAmount) {
     this.countOfTrade += 1
     this.tradeVolume += buyAmount
   }
 
-  // internalBalanceTransfer(amount, { from, to }) {
-  //   const senderAccountId = from
-  //   const receiverAccountId = to
-  //   senderAccountId.
-  // }
+  internalBalanceTransfer(amount, { from, to }) {
+    const senderAccountId = from
+    const receiverAccountId = to
+    //balance transfer withdraw leg
+    for (let i = 0; i < this.accounts.length; i++) {
+      if (this.accounts[i].accId === senderAccountId) {
+        this.accounts[i].withdraw(amount)
+      }
+    }
+    //balance transfer deposit leg
+    for (let j = 0; j < this.accounts.length; j++) {
+      if (this.accounts[j].accId === receiverAccountId) {
+        this.accounts[j].deposit(amount)
+      }
+    }
+  }
+  externalBalanceTransfer(amount, { from, to }) {
+    const senderAccountId = from
+    const receiverAccountId = to
+    if (senderAccountId === null) {
+      for (let i = 0; i < this.accounts.length; i++) {
+        if (this.accounts[i].accId === receiverAccountId) {
+          this.accounts[i].deposit(amount)
+        }
+      }
+    } else if (receiverAccountId === null) {
+      for (let j = 0; j < this.accounts.length; j++) {
+        if (this.accounts[j] === senderAccountId) {
+          this.accounts[j].withdraw(amount)
+        }
+      }
+    }
+  } //need to talk about logic
+  updateSettings() {}
 
   static create(name) {
     const user = new User(name)
