5815d816ee347f48c0e718d61cc9162438a38f04
Nur Digdem Deyirmenci <150827069+DigdemD@users.noreply.github.com>
2024-01-14 17:35:25 +0000

    Add external Balance Transfer API Test to ensure the correctness ofnewly updated balance of an account

--- backend/tests/api.test.js
+++ backend/tests/api.test.js
@@ -66,7 +66,28 @@ describe('Test the api', () => {
     const response = await request(app)
       .put(`/accounts/${accountOfCharlesNo1.body._id}`)
       .send({
-        newValues: { status: 'xxx' },
+        newValues: { status: 'Passive' },
       })
+  })
+  test('should  manage external balance  transfer', async () => {
+    const charles = await request(app).post('/users').send({
+      name: 'Charles',
+      surname: 'Darwin',
+    })
+    const accountOfCharlesNo1 = await request(app).post('/accounts').send({
+      ownerId: charles.body._id,
+      currency: 'EUR',
+    })
+    const accountOfCharlesNo2 = await request(app).post('/accounts').send({
+      ownerId: charles.body._id,
+      currency: 'USD',
+    })
+
+    const expectedOutput = { _id: accountOfCharlesNo1.body._id, owner: charles.body, status: 'Passive' }
+    const response = await request(app).post(`/users/${charles.body._id}/transfers`).send({
+      senderAccountId: null,
+      receiverAccountId: accountOfCharlesNo1.body._id,
+      amount: 500000,
+    })
   })
 })
